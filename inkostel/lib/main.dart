3d66526b35cc16c007fde2c80c28d5823555635e
ahmdmuflih <ahmdmuflih@student.telkomuniversity.ac.id>
2024-03-08 17:08:54 +0700

    test

--- inkostel/lib/main.dart
+++ inkostel/lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Chandra Kontrol'),
+      home: const MyHomePage(title: 'haiya hafiq lanciao, tembak betul betul looo'),
     );
   }
 }
