df68c65c40cde7ad336493a9460bd2a30ddbe962
RIPO12345 <placida.ripo.naibaho@gmail.com>
2024-03-16 15:44:43 +0700

    Cari kos

--- inkostel/lib/main.dart
+++ inkostel/lib/main.dart
@@ -12,11 +12,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        fontFamily: 'Schyler',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          fontFamily: 'Schyler',
         ),
-      home: const CariKos()
-    );
+        home: CariKos());
   }
 }
--- inkostel/lib/pages/carikos.dart
+++ inkostel/lib/pages/carikos.dart
@@ -1,34 +1,140 @@
 import 'package:flutter/material.dart';
 
-class CariKos extends StatelessWidget {
-  const CariKos({Key? key}) : super(key: key);
+void main() {
+  runApp(CariKos());
+}
 
+class CariKos extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Cari Kos'),
-        backgroundColor: const Color.fromRGBO(231, 246, 255, 1)
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            color:const Color.fromRGBO(39, 188, 49, 1),
-            height: 100,
-            child: const Text(
-              "Container"
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Home'),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {
+                // Action when search icon is pressed
+              },
             ),
-            
+            IconButton(
+              icon: Icon(Icons.save),
+              onPressed: () {
+                // Action when save icon is pressed
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () {
+                // Action when add icon is pressed
+              },
+            ),
+          ],
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              padding: EdgeInsets.all(16.0),
+              child: Row(
+                children: [
+                  // Logo profile di sini
+                  Expanded(
+                    child: Text('Logo Profile'),
+                  ),
+                  // Kolom search di sini
+                  Expanded(
+                    flex: 3,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Search...',
+                      ),
+                    ),
+                  ),
+                  // Icon filter di sini
+                  IconButton(
+                    icon: Icon(Icons.filter_list),
+                    onPressed: () {
+                      // Action when filter icon is pressed
+                    },
+                  ),
+                ],
+              ),
+            ),
+            // Tiga tombol terdekat, termurah, dan rekomendasi
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    // Action when nearest button is pressed
+                  },
+                  child: Text('Nearest'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    // Action when cheapest button is pressed
+                  },
+                  child: Text('Cheapest'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    // Action when recommended button is pressed
+                  },
+                  child: Text('Recommended'),
+                ),
+              ],
+            ),
+            // Persegi panjang di satu kolom
+            Container(
+              height: 200,
+              color: Colors.blue,
+              child: Center(
+                child: Text('Square 1'),
+              ),
+            ),
+            // Persegi panjang di kolom sendiri
+            Container(
+              height: 200,
+              color: Colors.green,
+              child: Center(
+                child: Text('Square 2'),
+              ),
+            ),
+          ],
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              IconButton(
+                icon: Icon(Icons.home),
+                onPressed: () {
+                  // Action when home icon is pressed
+                },
+              ),
+              IconButton(
+                icon: Icon(Icons.search),
+                onPressed: () {
+                  // Action when search icon is pressed
+                },
+              ),
+              IconButton(
+                icon: Icon(Icons.save),
+                onPressed: () {
+                  // Action when save icon is pressed
+                },
+              ),
+              IconButton(
+                icon: Icon(Icons.add),
+                onPressed: () {
+                  // Action when add icon is pressed
+                },
+              ),
+            ],
           ),
-          Expanded(child: Container(
-            color:const Color.fromRGBO(171, 81, 204, 1),
-            child: const Text(
-              "Expanded"
-            )
-            
-          ))
-        ],
+        ),
       ),
     );
   }
