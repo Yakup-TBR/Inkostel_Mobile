0acc5d190dd5d79598d0d80132824d42d49c6a77
Yakup-TBR <yakupasmaidyatanggae@gmail.com>
2024-03-18 03:13:28 +0800

    g

--- inkostel/lib/main.dart
+++ inkostel/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:inkostel/pages/carikos.dart';
 import 'package:inkostel/pages/detail.dart';
+import 'package:inkostel/pages/simpan.dart';
+import 'package:inkostel/pages/validation.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,6 +19,6 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           fontFamily: 'Schyler',
         ),
-        home: detail());
+        home: Validation());
   }
 }
