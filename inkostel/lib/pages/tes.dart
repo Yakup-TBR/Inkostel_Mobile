50ca05d9e302a8e92b478316f783fd8a7ed8d5c2
Yakup-TBR <yakupasmaidyatanggae@gmail.com>
2024-03-22 20:44:23 +0800

    Add appbar, searchbar, bottom nav bar

--- inkostel/lib/pages/tes.dart
+++ inkostel/lib/pages/tes.dart
@@ -1,7 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-
 void main() {
   runApp(const Tes());
 }
@@ -14,7 +11,7 @@ class Tes extends StatelessWidget {
     return MaterialApp(
         home: Scaffold(
       backgroundColor: const Color.fromRGBO(173, 188, 159, 1),
-      appBar: AppBar(
+      appBar: AppBar( // ----------Appbar 
         toolbarHeight: 100,
         backgroundColor: const Color.fromRGBO(173, 188, 159, 1),
         title: Padding(
@@ -33,7 +30,7 @@ class Tes extends StatelessWidget {
                           const Color.fromARGB(255, 0, 0, 0).withOpacity(0.5),
                       spreadRadius: 0,
                       blurRadius: 4,
-                      offset: const Offset(0, 1), // changes position of shadow
+                      offset: const Offset(0, 1), // Atur posisi shadow
                     ),
                   ],
                 ),
@@ -56,74 +53,113 @@ class Tes extends StatelessWidget {
       ),
       body: Stack(
         children: [
-          Container(
-            // SearchBar dan Tombol
-            child: Column(
+          Column(children: [ // ----------SearchBar dan Tombol
+            Row(
               children: [
-                Row(
-                  children: [
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 30),
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: const Color.fromRGBO(254, 251, 246, 1),
-                            borderRadius: BorderRadius.circular(32),
-                          ),
-                          child: const TextField(
-                            decoration: InputDecoration(
-                              hintStyle: TextStyle(fontSize: 17),
-                              hintText: 'Cari Kos Disini..',
-                              suffixIcon: Icon(Icons.search),
-                              border: InputBorder.none,
-                              contentPadding: EdgeInsets.only(left:20, top:10),
-                            ),
-                          ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 30),
+                    child: Container(
+                      height: 45,
+                      decoration: BoxDecoration(
+                        color: const Color.fromRGBO(254, 251, 246, 1),
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: const TextField(
+                        decoration: InputDecoration(
+                          hintStyle: TextStyle(fontSize: 17),
+                          hintText: 'Cari Kos Disini..',
+                          suffixIcon: Icon(Icons.search),
+                          border: InputBorder.none,
+                          contentPadding: EdgeInsets.only(left: 20, top: 8),
                         ),
                       ),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 20, right: 20),
-                      child: Container(
-                        width: 42,
-                        height: 42,
-                        decoration: BoxDecoration(
-                          color: const Color.fromARGB(255, 255, 255, 255),
-                          borderRadius: BorderRadius.circular(8),
-                          boxShadow: [
-                            BoxShadow(
-                              color: const Color.fromARGB(255, 0, 0, 0)
-                                  .withOpacity(0.5),
-                              spreadRadius: 0,
-                              blurRadius: 4,
-                              offset: const Offset(
-                                  0, 1), // changes position of shadow
-                            ),
-                          ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20, right: 20),
+                  child: Container(
+                    width: 40,
+                    height: 40,
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 255, 255, 255),
+                      borderRadius: BorderRadius.circular(8),
+                      boxShadow: [
+                        BoxShadow(
+                          color: const Color.fromARGB(255, 0, 0, 0)
+                              .withOpacity(0.5),
+                          spreadRadius: 0,
+                          blurRadius: 4,
+                          offset:
+                              const Offset(0, 1), // Atur posisi shadow
                         ),
-                        child: IconButton(
-                          icon: const Icon(Icons.filter_list),
-                          onPressed: () {
-                            // atur filter
-                          },
-                        ),
-                      ),
+                      ],
                     ),
-                  ],
+                    padding: const EdgeInsets.all(7),
+                    child: Image.asset(
+                      'lib/icons/filter.png',
+                      color: const Color.fromRGBO(67, 104, 80, 1),
+                    ),
+                  ),
                 ),
                 const Row()
               ],
             ),
-          ),
+          ]),
           Container(// Tombol Terdekat Termurah Rekomendasi
 
               ),
           Container(// Isi
 
               ),
-          Container(// Bottom Navigation Bar
-
-              )
+          Container( // ----------Bottom Navigation Bar
+            height: 65,
+            margin: const EdgeInsets.only(
+              // Harus diatur per halaman R,L,T,B nya
+              right: 50,
+              left: 50,
+              top: 430,
+              bottom: 100,
+            ),
+            decoration: BoxDecoration(
+              color: const Color.fromRGBO(67, 104, 80, 1),
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: [
+                BoxShadow(
+                  color: const Color.fromARGB(255, 0, 0, 0).withOpacity(0.5),
+                  spreadRadius: 0,
+                  blurRadius: 4,
+                  offset: const Offset(0, 1), // changes position of shadow
+                ),
+              ],
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Image.asset(
+                  height: 35,
+                  'lib/icons/home.png',
+                  color: const Color.fromARGB(255, 232, 255, 240),
+                ),
+                Image.asset(
+                  'lib/icons/search.png',
+                  height: 35,
+                  color: const Color.fromARGB(255, 232, 255, 240),
+                ),
+                Image.asset(
+                  height: 35,
+                  'lib/icons/simpan.png',
+                  color: const Color.fromARGB(255, 232, 255, 240),
+                ),
+                Image.asset(
+                  height: 35,
+                  'lib/icons/plus.png',
+                  color: const Color.fromARGB(255, 232, 255, 240),
+                ),
+              ],
+            ),
+          )
         ],
       ),
     ));
