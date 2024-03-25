e831f37053e4f9e16421757e3a5d4e4a9362ad3d
Yakup-TBR <yakupasmaidyatanggae@gmail.com>
2024-03-25 23:58:21 +0800

    Add SplashScreen

--- inkostel/lib/main.dart
+++ inkostel/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:inkostel/pages/profile.dart';
+import 'package:inkostel/pages/splash.dart';
 import 'package:inkostel/pages/tes.dart';
 import 'package:inkostel/pages/login.dart';
 import 'package:inkostel/pages/carikos.dart';
@@ -23,6 +24,6 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           fontFamily: 'Poppins',
         ),  
-        home:  const Tes());
+        home:  const SplashScreen());
   }
 }
---
+++ inkostel/lib/pages/splash.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:inkostel/pages/tes.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> 
+  with SingleTickerProviderStateMixin{
+
+    @override
+  void initState() {
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+
+    Future.delayed(const Duration(seconds: 3), (){
+Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_) => const Tes()));
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+    overlays: SystemUiOverlay.values);
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: const Color.fromRGBO(253, 252, 248, 1),
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+            Image.asset(
+                  'lib/icons/logo.png',
+                )
+          ],
+        ));
+  }
+}
