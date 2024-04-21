4e6ea85d52d088c7efc40b985581e8eee9385d8a
MNFI13-blode <naufaltaurus24@gmail.com>
2024-04-21 17:33:10 +0700

    Update upload gambar pada widget edit profil

--- inkostel/macos/Flutter/GeneratedPluginRegistrant.swift
+++ inkostel/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,11 +6,13 @@ import FlutterMacOS
 import Foundation
 
 import awesome_notifications
+import file_selector_macos
 import path_provider_foundation
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AwesomeNotificationsPlugin.register(with: registry.registrar(forPlugin: "AwesomeNotificationsPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
