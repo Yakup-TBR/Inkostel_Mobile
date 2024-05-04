1ec489a9377e252798e301bc8b1b1fcdc66149f2
Yakup-TBR <yakupasmaidyatanggae@gmail.com>
2024-05-05 00:01:31 +0800

    Update main.dart

--- inkostel/lib/main.dart
+++ inkostel/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:get/get.dart';
 import 'package:inkostel/pages/home.dart';
 import 'package:inkostel/pages/carikos.dart';
@@ -15,6 +17,9 @@ import 'package:inkostel/views/login.view.dart';
 import 'package:inkostel/views/splash.view.dart';
 
 void main() async {
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
   await AwesomeNotifications().initialize(null, [
     NotificationChannel(
         channelGroupKey: "grup_notifikasi",
