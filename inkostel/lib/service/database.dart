951d8679b32639ea1571b846959a3821f9c1661b
Yakup-TBR <yakupasmaidyatanggae@gmail.com>
2024-05-16 12:01:49 +0800

    Update database, iklan ke validasi

--- inkostel/lib/service/database.dart
+++ inkostel/lib/service/database.dart
@@ -4,7 +4,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class DatabaseMethods {
   Future addKosDetails(Map<String, dynamic> kosDataMap, String id) async {
     return await FirebaseFirestore.instance
-        .collection("Kos")
+        .collection("ValidasiKos")
         .doc(id)
         .set(kosDataMap);
   }
